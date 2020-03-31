1f9405a093fa5276a33931e3393fcf3044532065
Mamady Sillah <supurpisces310@msn.com>
2020-03-30 21:51:43 -0400

    did the fetch and console.log...was returned list of countries in console.

---
+++ src/containers/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+import '../containers/App.css';
+
+class App extends Component {
+constructor () {
+  super ()
+    this.state = {
+      countries: [],
+    }
+  
+}
+
+componentDidMount () {
+  fetch("https://api.covid19api.com/summary")
+  .then(response=> response.json())
+  .then(users=> {this.setState({countries: users})
+  console.log(users)
+});
+
+}  
+render () {
+  return (
+    <div> 
+   
+    </div>
+
+  )
+}
+}
+
+export default App
+
