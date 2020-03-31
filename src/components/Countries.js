3006af2eb79604c34583ed42fd913cdbbb91dcfa
Mamady Sillah <supurpisces310@msn.com>
2020-03-30 22:11:36 -0400

    completed countries .js

---
+++ src/components/Countries.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Countries = ({Country, TotalConfirmed, TotalDeaths, TotalRecovered}) => {
+return (
+    <div>
+        <h2>{Country}</h2>
+        <p>{TotalConfirmed}</p>
+        <p>{TotalDeaths}</p>
+        <p>{TotalRecovered}</p>
+    </div>
+  );
+}
